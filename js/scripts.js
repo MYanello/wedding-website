94b3a48f75546add448e949ca5d0273ecbbfd5b9
myanello-rescale <myanello@rescale.com>
2024-08-11 16:15:01 -0500

    update md5

--- js/scripts.js
+++ js/scripts.js
@@ -214,8 +214,7 @@ $(document).ready(function () {
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+        if (MD5($('#invite_code').val()) !== 'f3a3cc4e1b8b4b0438505c0a38efad9f') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbzuJ1rLzWE1FLyhbkTY9qrRAXoOHfWJdPwS7FnjyaA9n--PAUmA7jNSriotixez_U0/exec', data)
