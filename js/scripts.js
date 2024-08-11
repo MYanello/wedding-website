fe8ea115cb254b0691079444181adefc528f3787
myanello-rescale <myanello@rescale.com>
2024-08-11 13:50:17 -0500

    more absic edis

--- js/scripts.js
+++ js/scripts.js
@@ -215,7 +215,7 @@ $(document).ready(function () {
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
         if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== 'c4ca4238a0b923820dcc509a6f75849b') {
+            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
             $.post('https://script.google.com/macros/s/AKfycbzuJ1rLzWE1FLyhbkTY9qrRAXoOHfWJdPwS7FnjyaA9n--PAUmA7jNSriotixez_U0/exec', data)
